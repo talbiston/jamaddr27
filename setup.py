d203ee7487d2faa209f215173a04285a9a343358
talbiston <foxtrot711@gmail.com>
2018-05-31 18:17:29 -0500

    change

--- setup.py
+++ setup.py
@@ -1,30 +1,7 @@
 from setuptools import setup, find_packages
 
-long_description = """# Jamaddr Python Package
-
-This package Created by Jamison Emilio and packaged by Todd Albiston
-
-## Description:
-Python Package adds functions to convert IP address or subnet masks to bits and bit back up IP address.
-
-
-```python
-from jamaddr import JamAddr
-ipbits = JamAddr.ip_to_bits("192.168.0.23")
-ipbits
-```
-
-returns = '11000000101010000000000000010111'
-
-```python
-from jamaddr import JamAddr
-bitsip = JamAddr.bits_to_ip("11000000101010000000000000010111")
-bitsip
-```
-
-returns = '192.168.0.23'
-
-#"""
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(
     name='jamaddr27',
